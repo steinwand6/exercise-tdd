971b086a0e11ee1da2bf1fd40d4e871c5d54c50b
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-05-23 02:18:28 +0900

    move test to main from dollar

--- multi_currency_money/src/dollar.rs
+++ multi_currency_money/src/dollar.rs
@@ -18,21 +18,3 @@ impl Dollar {
         self.amount == obj.amount
     }
 }
-
-#[cfg(test)]
-mod dollar_test {
-    use crate::dollar::Dollar;
-
-    #[test]
-    fn test_mutlipulication() {
-        let five = Dollar::new(5);
-        assert_eq!(five.times(2), Dollar::new(10));
-        assert_eq!(five.times(3), Dollar::new(15));
-    }
-
-    #[test]
-    fn test_equality() {
-        assert!(Dollar::new(5).equal(Dollar::new(5)));
-        assert!(!Dollar::new(5).equal(Dollar::new(6)));
-    }
-}
--- multi_currency_money/src/main.rs
+++ multi_currency_money/src/main.rs
@@ -14,3 +14,21 @@ mod dollar;
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod money_test {
+    use crate::dollar::Dollar;
+
+    #[test]
+    fn test_mutlipulication() {
+        let five = Dollar::new(5);
+        assert_eq!(five.times(2), Dollar::new(10));
+        assert_eq!(five.times(3), Dollar::new(15));
+    }
+
+    #[test]
+    fn test_equality() {
+        assert!(Dollar::new(5).equal(Dollar::new(5)));
+        assert!(!Dollar::new(5).equal(Dollar::new(6)));
+    }
+}
