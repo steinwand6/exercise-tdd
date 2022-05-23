0cc0c7892f2d657f375cec88d188a73c880f75c3
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-05-23 20:18:47 +0900

    Merge equal test

--- multi_currency_money/src/main.rs
+++ multi_currency_money/src/main.rs
@@ -10,8 +10,9 @@
 // [ ] equality comparison with Other Object
 // [x] 5CHF * 2 = 10CHF
 // [ ] overlapping dollar and franc
-// [ ] generalization of equal()
+// [x] generalization of equal()
 // [ ] generalization of times()
+// [ ] Compare Dollar and Franc
 
 mod money;
 
@@ -36,6 +37,9 @@ mod money_test {
     fn test_equality() {
         assert!(Dollar::new(5).equal(Dollar::new(5)));
         assert!(!Dollar::new(5).equal(Dollar::new(6)));
+        assert!(Franc::new(5).equal(Franc::new(5)));
+        assert!(!Franc::new(5).equal(Franc::new(6)));
+        assert!(!Franc::new(5).equal(Dollar::new(5)));
     }
 
     #[test]
@@ -43,11 +47,5 @@ mod money_test {
         let five = Franc::new(5);
         assert_eq!(five.times(2), Franc::new(10));
         assert_eq!(five.times(3), Franc::new(15));
-    }
-
-    #[test]
-    fn test_franc_equality() {
-        assert!(Franc::new(5).equal(Franc::new(5)));
-        assert!(!Franc::new(5).equal(Franc::new(6)));
     }
 }
