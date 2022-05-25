bb71ad7a913f020f5b1cee04f091fc4243d55134
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-05-26 00:52:16 +0900

    Move test to money.rs from main.rs

--- multi_currency_money/src/money/money.rs
+++ multi_currency_money/src/money/money.rs
@@ -89,11 +89,40 @@ impl Money {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::Currency;
-    use super::Money;
-    use super::Sum;
+mod money_test {
     use crate::bank::bank::Bank;
+    use crate::money::money::{Currency, Money, Sum};
+
+    #[test]
+    fn test_mutlipulication() {
+        let five = Money::dollar(5);
+        assert_eq!(five.times(2), Money::dollar(10));
+        assert_eq!(five.times(3), Money::dollar(15));
+    }
+
+    #[test]
+    fn test_equality() {
+        assert!(Money::dollar(5).equal(Money::dollar(5)));
+        assert!(!Money::dollar(5).equal(Money::dollar(6)));
+        assert!(!Money::franc(5).equal(Money::dollar(5)));
+    }
+
+    #[test]
+    fn test_simple_addition() {
+        let bank = Bank::new();
+        let five = Money::dollar(5);
+        let sum = five.plus(&Money::dollar(5));
+        let reduced = bank.reduce(sum, Currency::USD);
+        assert_eq!(reduced, Money::dollar(10));
+    }
+
+    #[test]
+    fn test_plus_returns_sum() {
+        let five = Money::dollar(5);
+        let sum = five.plus(&five);
+        assert_eq!(Money::dollar(5), sum.augend);
+        assert_eq!(Money::dollar(5), sum.addend);
+    }
 
     #[test]
     fn test_reduce_sum() {
